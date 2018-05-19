2c573609d5203140d21398c6f3ad6563156829fe
ProjectGitHub72 <projectemail72@gmail.com>
2018-05-19 14:51:42 +0545

    tring to add something

---
+++ Program.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.first;
+
+/**
+ *
+ * @author julia dhakal
+ */
+public class Program {
+
+    public static void main(String[] args) {
+
+       
+        
+        System.out.println("hello");
+        System.out.print("hello");
+        System.out.println("hello");
+     
+    }
+
+}
